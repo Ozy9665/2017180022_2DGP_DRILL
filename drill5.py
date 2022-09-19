12998ca59a3d7edd1fa361287130616263ec4270
OJW <dnjsrk9665@naver.com>
2022-09-19 18:51:26 +0900

    complete drill05
    drill5 완성

--- drill5.py
+++ drill5.py
@@ -1,2 +1,36 @@
 import turtle
 
+turtle.reset()
+turtle.shape('turtle')
+
+def up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.onkey(up, 'w')
+turtle.onkey(left, 'a')
+turtle.onkey(down, 's')
+turtle.onkey(right, 'd')
+
+turtle.onkey(restart, 'Escape')
+
+turtle.listen()
